36ea340d8aecc7305291e6ed152d7c909de60f52
mariliab <marilia.bognandi@gmail.com>
2020-10-21 17:45:50 +0200

    new component views and functionality to delete educations

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,3 +3,17 @@ from django.db import models
 class UserModel(models.Model):
     name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class EducationModel(models.Model):
+    name = models.CharField(max_length=100)
+    education_type = models.CharField(max_length=100)
+    education_length = models.CharField(max_length=100)
+    education_pace = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
--- myproject/myapp/schema.py
+++ myproject/myapp/schema.py
@@ -1,17 +1,29 @@
 import graphene
+from django.shortcuts import get_object_or_404
 from graphene_django import DjangoObjectType
-
-from myapp.models import UserModel
+from myapp.models import UserModel, EducationModel
 
 class UserType(DjangoObjectType):
     class Meta:
         model = UserModel
 
+class EducationType(DjangoObjectType):
+    class Meta:
+        model = EducationModel
+
 class Query(graphene.ObjectType):
     users = graphene.List(UserType)
+    educations = graphene.List(EducationType)
+    education_by_id = graphene.Field(EducationType, id=graphene.Int())
 
     def resolve_users(self, info):
         return UserModel.objects.all()
+
+    def resolve_educations(self, info):
+        return EducationModel.objects.all()
+
+    def resolve_educations_by_id(self, info, id):
+        return get_object_or_404(EducationModel, pk=id)
 
 class CreateUser(graphene.Mutation):
     id = graphene.Int()
@@ -32,8 +44,49 @@ class CreateUser(graphene.Mutation):
             last_name=user.last_name,
         )
 
+class CreateEducation(graphene.Mutation):
+    id = graphene.Int()
+    name = graphene.String()
+    education_type = graphene.String()
+    education_length = graphene.String()
+    education_pace = graphene.String()
+    description = graphene.String()
+
+    class Arguments:
+        name = graphene.String()
+        education_type = graphene.String()
+        education_length = graphene.String()
+        education_pace = graphene.String()
+        description = graphene.String()
+
+    def mutate(self, info, name, education_type, education_length, education_pace, description):
+        education = EducationModel(name=name, education_type=education_type, education_length=education_length, education_pace=education_pace, description=description)
+        education.save()
+
+        return CreateEducation(
+            id=education.id,
+            name=education.name, 
+            education_type=education.education_type, 
+            education_length=education.education_length, 
+            education_pace=education.education_pace, 
+            description=education.description
+        )
+
+class DeleteEducation(graphene.Mutation):
+    id = graphene.Int()
+
+    class Arguments:
+        id = graphene.Int()
+
+    def mutate(self, info, id):
+        education = get_object_or_404(EducationModel, pk=id)
+        education.delete()
+        return DeleteEducation(id=education.id)
+
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
+    create_education = CreateEducation.Field()
+    delete_education = DeleteEducation.Field()
     
 schema = graphene.Schema(
     query=Query,
